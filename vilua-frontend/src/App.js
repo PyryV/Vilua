b86aa5216b14566bf18f410f2f5ab097ce6bc239
PyryV <pyry.vekkeli@helsinki.fi>
2019-03-14 17:10:19 +0200

    started creating login

--- vilua-frontend/src/App.js
+++ vilua-frontend/src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const Login = () => {
+  return (
+    <div>
+      <form id='login' >
+        Käyttäjätunnus
+        Salasana
+        
+      </form>
+    </div>
+  )
+}
+
+const Menu = () => {
+  return (
+    <div className='Menu'>
+      Main menu
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Vilua ry</h1>
+      <Menu />
+    </div>
+  )
 }
 
 export default App;
